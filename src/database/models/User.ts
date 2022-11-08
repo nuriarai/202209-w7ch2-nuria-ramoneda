74c39977d1b92bdfea7d8dbcd53b274c33aff73b
Núria Ramoneda <nuriarai@gmail.com>
2022-11-08 21:46:43 +0100

    Add minLengt to password field and email field in model database

--- src/database/models/User.ts
+++ src/database/models/User.ts
@@ -2,13 +2,19 @@ import { Schema, model } from "mongoose";
 
 const userSchema = new Schema({
   username: {
+    type: String,
     required: true,
     unique: true,
-    type: String,
   },
   password: {
-    required: true,
     type: String,
+    required: true,
+    minLength: 4,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
   },
 });
 
